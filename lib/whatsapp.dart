9716aa5c8dd695cca13daeed9959781e2ef4d8c0
minhazma <53335392+minhazma@users.noreply.github.com>
2023-12-12 12:21:23 +0530

    template_updated

--- lib/whatsapp.dart
+++ lib/whatsapp.dart
@@ -16,10 +16,7 @@ class WhatsApp {
   setup({accessToken, int? fromNumberId}) {
     _accessToken = accessToken;
     _fromNumberId = fromNumberId;
-    _headers = {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer $_accessToken"
-    };
+    _headers = {"Content-Type": "application/json", "Authorization": "Bearer $_accessToken"};
   }
 
   /// Generate the short link of the WhatsApp.
@@ -38,8 +35,8 @@ class WhatsApp {
   /// Send the template to the client.
   /// [to] is the phone number with country code but without the plus (+) sign.
   /// [templateName] is the template name.
-  Future messagesTemplate({int? to, String? templateName}) async {
-    var url = 'https://graph.facebook.com/v14.0/$_fromNumberId/messages';
+  Future messagesTemplateText({int? to, String? templateName, String? languageCode, List? components}) async {
+    var url = 'https://graph.facebook.com/v18.0/$_fromNumberId/messages';
     Uri uri = Uri.parse(url);
 
     Map data = {
@@ -48,7 +45,8 @@ class WhatsApp {
       "type": "template",
       "template": {
         "name": templateName,
-        "language": {"code": "en_US"}
+        "language": {"code": languageCode ?? "en_US"},
+        "components":components
       }
     };
 
@@ -71,7 +69,7 @@ class WhatsApp {
     String? message,
     bool? previewUrl,
   }) async {
-    var url = 'https://graph.facebook.com/v14.0/$_fromNumberId/messages';
+    var url = 'https://graph.facebook.com/v18.0/$_fromNumberId/messages';
     Uri uri = Uri.parse(url);
 
     Map data = {
@@ -97,7 +95,7 @@ class WhatsApp {
   /// [mediaType] is the type of media such as image, document, sticker, audio or video
   /// [mediaId] use this edge to retrieve and delete media.
   Future messagesMedia({to, mediaType, mediaId}) async {
-    var url = 'https://graph.facebook.com/v14.0/$_fromNumberId/messages';
+    var url = 'https://graph.facebook.com/v18.0/$_fromNumberId/messages';
     Uri uri = Uri.parse(url);
     Map data = {
       "messaging_product": "whatsapp",
@@ -124,19 +122,14 @@ class WhatsApp {
   /// [name] is the name of the location.
   /// [address] is the full address of the location.
   Future messagesLocation({to, longitude, latitude, name, address}) async {
-    var url = 'https://graph.facebook.com/v14.0/$_fromNumberId/messages';
+    var url = 'https://graph.facebook.com/v18.0/$_fromNumberId/messages';
     Uri uri = Uri.parse(url);
 
     Map data = {
       "messaging_product": "whatsapp",
       "to": to,
       "type": "location",
-      "location": {
-        "longitude": longitude,
-        "latitude": latitude,
-        "name": name,
-        "address": address
-      }
+      "location": {"longitude": longitude, "latitude": latitude, "name": name, "address": address}
     };
 
     var body = json.encode(data);
@@ -155,7 +148,7 @@ class WhatsApp {
   /// [mediaLink] is media to be sent.
   /// [caption] is caption of media
   Future messagesMediaByLink({to, mediaType, mediaLink, caption}) async {
-    var url = 'https://graph.facebook.com/v14.0/$_fromNumberId/messages';
+    var url = 'https://graph.facebook.com/v18.0/$_fromNumberId/messages';
 
     Map data = {
       "messaging_product": "whatsapp",
@@ -167,8 +160,7 @@ class WhatsApp {
 
     var body = json.encode(data);
 
-    var response =
-        await http.post(Uri.parse(url), headers: _headers, body: body);
+    var response = await http.post(Uri.parse(url), headers: _headers, body: body);
     try {
       return json.decode(response.body);
     } catch (e) {
@@ -181,7 +173,7 @@ class WhatsApp {
   /// [messageId] is the message id.
   /// [emoji] is the emoji to be sent.
   Future messagesReaction({to, messageId, emoji}) async {
-    var url = 'https://graph.facebook.com/v14.0/$_fromNumberId/messages';
+    var url = 'https://graph.facebook.com/v18.0/$_fromNumberId/messages';
 
     Map data = {
       "messaging_product": "whatsapp",
@@ -193,8 +185,7 @@ class WhatsApp {
 
     var body = json.encode(data);
 
-    var response =
-        await http.post(Uri.parse(url), headers: _headers, body: body);
+    var response = await http.post(Uri.parse(url), headers: _headers, body: body);
     try {
       return json.decode(response.body);
     } catch (e) {
@@ -208,7 +199,7 @@ class WhatsApp {
   /// [message] is the message to be sent.
   /// [previewUrl] is used to preview the URL in the chat window.
   Future messagesReply({to, messageId, previewUrl, message}) async {
-    var url = 'https://graph.facebook.com/v14.0/$_fromNumberId/messages';
+    var url = 'https://graph.facebook.com/v18.0/$_fromNumberId/messages';
 
     Map data = {
       "messaging_product": "whatsapp",
@@ -221,8 +212,7 @@ class WhatsApp {
 
     var body = json.encode(data);
 
-    var response =
-        await http.post(Uri.parse(url), headers: _headers, body: body);
+    var response = await http.post(Uri.parse(url), headers: _headers, body: body);
     try {
       return json.decode(response.body);
     } catch (e) {
@@ -236,7 +226,7 @@ class WhatsApp {
   /// [mediaType] is type of media such as image, document, sticker, audio or video
   /// [mediaId] is id of media to be replay.
   Future messagesReplyMedia({to, messageId, mediaType, mediaId}) async {
-    var url = 'https://graph.facebook.com/v14.0/$_fromNumberId/messages';
+    var url = 'https://graph.facebook.com/v18.0/$_fromNumberId/messages';
 
     Map data = {
       "messaging_product": "whatsapp",
@@ -249,8 +239,7 @@ class WhatsApp {
 
     var body = json.encode(data);
 
-    var response =
-        await http.post(Uri.parse(url), headers: _headers, body: body);
+    var response = await http.post(Uri.parse(url), headers: _headers, body: body);
     try {
       return json.decode(response.body);
     } catch (e) {
@@ -264,9 +253,8 @@ class WhatsApp {
   /// [mediaType] is type of media such as image, document, sticker, audio or video
   /// [mediaLink] is link of media to be sent.
   /// [caption] is caption of media to be sent.
-  Future messagesReplyMediaUrl(
-      {to, messageId, mediaType, mediaLink, caption}) async {
-    var url = 'https://graph.facebook.com/v14.0/$_fromNumberId/messages';
+  Future messagesReplyMediaUrl({to, messageId, mediaType, mediaLink, caption}) async {
+    var url = 'https://graph.facebook.com/v18.0/$_fromNumberId/messages';
 
     Map data = {
       "messaging_product": "whatsapp",
@@ -279,8 +267,7 @@ class WhatsApp {
 
     var body = json.encode(data);
 
-    var response =
-        await http.post(Uri.parse(url), headers: _headers, body: body);
+    var response = await http.post(Uri.parse(url), headers: _headers, body: body);
     try {
       return json.decode(response.body);
     } catch (e) {
@@ -291,14 +278,13 @@ class WhatsApp {
   /// Register a phone number
   /// [pin] is 6-digit pin for register number.
   Future registerNumber({pin}) async {
-    var url = 'https://graph.facebook.com/v14.0/$_fromNumberId/register';
+    var url = 'https://graph.facebook.com/v18.0/$_fromNumberId/register';
 
     Map data = {"messaging_product": "whatsapp", "pin": pin};
 
     var body = json.encode(data);
 
-    var response =
-        await http.post(Uri.parse(url), headers: _headers, body: body);
+    var response = await http.post(Uri.parse(url), headers: _headers, body: body);
     try {
       return json.decode(response.body);
     } catch (e) {
@@ -309,14 +295,13 @@ class WhatsApp {
   /// Set Two Step Verification Code
   /// [pin] is 6-digit pin for two step verification.
   Future setTwoStepVerification({pin}) async {
-    var url = 'https://graph.facebook.com/v14.0/$_fromNumberId/register';
+    var url = 'https://graph.facebook.com/v18.0/$_fromNumberId/register';
 
     Map data = {"pin": pin};
 
     var body = json.encode(data);
 
-    var response =
-        await http.post(Uri.parse(url), headers: _headers, body: body);
+    var response = await http.post(Uri.parse(url), headers: _headers, body: body);
     try {
       return json.decode(response.body);
     } catch (e) {
@@ -327,14 +312,13 @@ class WhatsApp {
   /// Deregister a phone number
   /// [pin] is 6-digit pin for deregister number.
   Future deregisterNumber({pin}) async {
-    var url = 'https://graph.facebook.com/v14.0/$_fromNumberId/deregister';
+    var url = 'https://graph.facebook.com/v18.0/$_fromNumberId/deregister';
 
     Map data = {"pin": pin};
 
     var body = json.encode(data);
 
-    var response =
-        await http.post(Uri.parse(url), headers: _headers, body: body);
+    var response = await http.post(Uri.parse(url), headers: _headers, body: body);
     try {
       return json.decode(response.body);
     } catch (e) {
@@ -345,8 +329,7 @@ class WhatsApp {
   /// Get Shared WhatsApp Business Account Id
   /// [inputToken] is token generated after embedding the signup flow
   Future getWhatsAppBusinessAccounts({inputToken}) async {
-    var url =
-        'https://graph.facebook.com/v14.0/debug_token?input_token=$inputToken';
+    var url = 'https://graph.facebook.com/v18.0/debug_token?input_token=$inputToken';
 
     var response = await http.get(Uri.parse(url), headers: _headers);
     try {
@@ -360,8 +343,7 @@ class WhatsApp {
   /// [accountId] is Business manager account Id
   Future getWhatsAppBusinessAccountsList({accountId}) async {
     var parseAccountId = accountId.toString();
-    var url =
-        'https://graph.facebook.com/v14.0/$parseAccountId/client_whatsapp_business_accounts';
+    var url = 'https://graph.facebook.com/v18.0/$parseAccountId/client_whatsapp_business_accounts';
 
     var response = await http.get(Uri.parse(url), headers: _headers);
     try {
@@ -376,7 +358,7 @@ class WhatsApp {
   /// [bodyText] is the main body text of message
   /// [buttons] is list of action buttons with id and text
   messagesButton({to, bodyText, buttons}) async {
-    var url = 'https://graph.facebook.com/v14.0/$_fromNumberId/messages';
+    var url = 'https://graph.facebook.com/v18.0/$_fromNumberId/messages';
 
     var buttonsList = [];
     for (var i = 0; i < buttons.length; i++) {
@@ -400,8 +382,7 @@ class WhatsApp {
 
     var body = json.encode(data);
 
-    var response =
-        await http.post(Uri.parse(url), headers: _headers, body: body);
+    var response = await http.post(Uri.parse(url), headers: _headers, body: body);
     try {
       return json.decode(response.body);
     } catch (e) {
@@ -413,15 +394,12 @@ class WhatsApp {
   ///[mediaFile] is the file object to be send
   ///[mediaName] is the name of file
   uploadMedia({required mediaFile, mediaType, mediaName}) async {
-    var uri =
-        Uri.parse('https://graph.facebook.com/v14.0/$_fromNumberId/media');
+    var uri = Uri.parse('https://graph.facebook.com/v18.0/$_fromNumberId/media');
 
     var request = http.MultipartRequest('POST', uri);
     request.headers.addAll(_headers!);
     request.fields['messaging_product'] = 'whatsapp';
-    request.files.add(http.MultipartFile.fromBytes(
-        'file', File(mediaFile.path).readAsBytesSync(),
-        filename: mediaName, contentType: mediaType));
+    request.files.add(http.MultipartFile.fromBytes('file', File(mediaFile.path).readAsBytesSync(), filename: mediaName, contentType: mediaType));
 
     var response = await request.send();
     final respStr = await response.stream.bytesToString();
@@ -435,7 +413,7 @@ class WhatsApp {
   /// Retrive URL of media
   /// [mediaId] is id of media file
   Future getMediaUrl({mediaId}) async {
-    var url = 'https://graph.facebook.com/v14.0/$mediaId';
+    var url = 'https://graph.facebook.com/v18.0/$mediaId';
 
     var response = await http.get(Uri.parse(url), headers: _headers);
     try {
@@ -448,7 +426,7 @@ class WhatsApp {
   /// Delete uploaded media
   /// [mediaId] is id of media file
   Future deleteMedia({mediaId}) async {
-    var url = 'https://graph.facebook.com/v14.0/$mediaId';
+    var url = 'https://graph.facebook.com/v18.0/$mediaId';
 
     var response = await http.delete(Uri.parse(url), headers: _headers);
     try {
@@ -466,16 +444,8 @@ class WhatsApp {
   /// [businessEmail] is email of your business
   /// [businessWebsites] is list of website to update
   /// [businessProfileId] is image handle id to update profile picture of business
-  Future updateProfile(
-      {businessAddress,
-      businessDescription,
-      businessIndustry,
-      businessAbout,
-      businessEmail,
-      required List businessWebsites,
-      businessProfileId}) async {
-    var url =
-        'https://graph.facebook.com/v14.0/$_fromNumberId/whatsapp_business_profile';
+  Future updateProfile({businessAddress, businessDescription, businessIndustry, businessAbout, businessEmail, required List businessWebsites, businessProfileId}) async {
+    var url = 'https://graph.facebook.com/v18.0/$_fromNumberId/whatsapp_business_profile';
 
     Map data = {
       "messaging_product": "whatsapp",
@@ -490,8 +460,7 @@ class WhatsApp {
 
     var body = json.encode(data);
 
-    var response =
-        await http.post(Uri.parse(url), headers: _headers, body: body);
+    var response = await http.post(Uri.parse(url), headers: _headers, body: body);
     try {
       return json.decode(response.body);
     } catch (e) {
